ec1e4a56bf8c6fddb12d3d323bd6de55962b0da5
seoyeun0106 <sarah.mun16@gmail.com>
2022-11-16 03:10:13 +0900

    Feat: footer 구현

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ReactComponent as MainIconGray } from '../assets/logos/image_logo_gray.svg';
-
 import styled from 'styled-components';
+
 export default function Footer() {
   const footerList = ['사이트 맵', '오시는 길', '주차 안내', '정보 공개', '개인정보 처리방침'];
   return (
@@ -14,7 +14,21 @@ export default function Footer() {
         </FooterList>
       </FooterNav>
       <FooterArticle>
-        <MainIconGray />
+        <MainIconGray margin-top="28px" />
+        <FooterBox>
+          <p>
+            {'('}우{')'} 06757 서울시 서초구 남부순환로 2406 예술의 전당
+          </p>
+          <p>
+            {' '}
+            대표자 | 장형준{'\n'} 사업자 등록번호 {'|'}
+            214-82-00264{'\n'} 통신판매업 신고 | 서초 제 0706호
+          </p>
+          <p>
+            예술의 전당 콜센터 : 02-580-1300 {'('}
+            09:00-20:00 | 연중 무휴{')'}
+          </p>
+        </FooterBox>
       </FooterArticle>
     </MainFooter>
   );
@@ -23,6 +37,13 @@ const MainFooter = styled.footer`
   height: 21rem;
   text-align: center;
   width: 100%;
+  * {
+    font-family: 'Pretendard';
+    font-style: normal;
+
+    font-size: 10px;
+    line-height: 143.52%;
+  }
 `;
 const FooterNav = styled.nav`
   display: flex;
@@ -42,10 +63,25 @@ const FooterItem = styled.li`
     margin: 11px;
   }
   font-weight: 500;
-  font-size: 10px;
-  line-height: 143.52%;
   color: #949494;
   flex-shrink: 0;
   height: 0.875rem;
 `;
-const FooterArticle = styled.article``;
+const FooterArticle = styled.article`
+  background-color: #131112;
+  height: 12.25rem;
+  .svg {
+    margin-top: 1.75rem;
+    margin-bottom: 1rem;
+  }
+`;
+const FooterBox = styled.div`
+  font-weight: 400;
+  white-space: pre-line;
+  text-align: center;
+  color: #62626a;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 5.25rem;
+`;

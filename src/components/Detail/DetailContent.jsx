29fb9d4198f9e79e71ef2ba0d7f986468f0f945d
myeongheonhong <ruddyh7076@gmail.com>
2022-11-21 16:15:34 +0900

    Feat : 공연 상세 이미지정보 컴포넌트 구현

--- src/components/Detail/DetailContent.jsx
+++ src/components/Detail/DetailContent.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CategoryNav from './CategoryNav';
 import PerformanceInfo from './PerformanceInfo';
+import PerformanceDetail from './PerformanceDetail';
 
 export default function DetailContent() {
   return (
     <div>
       <PerformanceInfo />
       <CategoryNav />
+      <PerformanceDetail />
     </div>
   );
 }
---
+++ src/components/Detail/PerformanceDetail.jsx
@@ -0,0 +1,93 @@
+import { React, useState } from 'react';
+import styled from 'styled-components';
+import DetailImage from '../../assets/logos/image_detail.svg';
+import DropDown from '../../assets/logos/icon_arrow_down.svg';
+
+export default function PerformanceDetail() {
+  const onClickViewMore = () => {
+    height === '691px' ? setHeight('100%') : setHeight('691px');
+  };
+
+  const [height, setHeight] = useState('691px');
+
+  return (
+    <StyledContainer>
+      <StyledWrap>
+        <StyledTitle>공지 사항</StyledTitle>
+        <StyledContent>
+          예술의 전당 유료회원(골드/블루/그린) : 20% 할인
+          <br />
+          예술의 전당 일반회원(싹틔우미, 노블) : 10% 할인
+        </StyledContent>
+      </StyledWrap>
+      <StyledWrap>
+        <StyledTitle>작품 설명</StyledTitle>
+        <StyledContent>
+          <StyledImageWrap src={DetailImage} alt="작품상세정보 이미지" height={height} />
+          <StyledViewMoreBtn onClick={onClickViewMore}>
+            <span>설명 더보기</span>
+            <StyledViewMoreIcon src={DropDown} alt="더보기" />
+          </StyledViewMoreBtn>
+        </StyledContent>
+      </StyledWrap>
+      <StyledWrap>
+        <StyledTitle>작가 소개</StyledTitle>
+        <StyledContent>
+          헬가 스텐첼은 영국에서 활동하는 예술가로, 일러스트, 사진, 비디오, 스톱 모션 애니메이션 등
+          다양한 매체와 미디어를 활용하여 &quot;집안의 초현실주의&quot;라는 주제로 일상에서 사용되는
+          오브제에 재치있는 아이디어로 생명력을 불어넣는 작업을 한다.
+        </StyledContent>
+        <StyledContent>
+          BBC, 혼다, O2 등 글로벌 브랜드와의 협업을 진행하였으며 2020년에는 &quot;올해의 푸드 아트
+          크리에이터&quot; 상을 수상했다.
+        </StyledContent>
+      </StyledWrap>
+    </StyledContainer>
+  );
+}
+
+const StyledContainer = styled.div`
+  margin-top: 30px;
+`;
+
+const StyledWrap = styled.div`
+  margin-top: 25px;
+`;
+
+const StyledTitle = styled.div`
+  font-size: 15px;
+  font-weight: 600;
+  line-height: 135%;
+  color: #232323;
+`;
+const StyledContent = styled.div`
+  font-size: 15px;
+  line-height: 140%;
+  margin-top: 8px;
+  color: #000000;
+`;
+
+const StyledImageWrap = styled.img`
+  width: 375px;
+  height: ${(height) => height}
+  object-fit: cover;
+  object-position: left top;
+`;
+
+const StyledViewMoreBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 375px;
+  height: 55px;
+  font-size: 15px;
+  line-height: 17.9px;
+  padding: 0;
+  background-color: #e2e2e2;
+  color: #949494;
+  border: none;
+`;
+
+const StyledViewMoreIcon = styled.img`
+  margin-left: 7px;
+`;

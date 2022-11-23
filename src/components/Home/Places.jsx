506b02177b505b84990f3caa2c73c3374dc58a43
seoyeun0106 <sarah.mun16@gmail.com>
2022-11-23 19:15:23 +0900

    feat: 티켓 오픈, 공연 전시, 공간 둘러보기 구현

---
+++ src/components/Home/Places.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as PlaceIcon2 } from '../../assets/logos/icon_오페라하우스.svg';
+import { ReactComponent as PlaceIcon1 } from '../../assets/logos/icon_음악당.svg';
+import { ReactComponent as PlaceIcon3 } from '../../assets/logos/icon_전시장.svg';
+export default function Places() {
+  return (
+    <StyledPlaces>
+      <StyledTitle>공간 둘러보기</StyledTitle>
+      <PlacesContainer>
+        <PlaceItem>
+          <div>
+            <PlaceIcon1></PlaceIcon1>
+          </div>
+          <StyledFont>음악당</StyledFont>
+        </PlaceItem>
+        <PlaceItem>
+          <div>
+            <PlaceIcon2></PlaceIcon2>
+          </div>
+          <StyledFont>오페라하우스</StyledFont>
+        </PlaceItem>
+        <PlaceItem>
+          <div>
+            <PlaceIcon3></PlaceIcon3>
+          </div>
+          <StyledFont>전시장</StyledFont>
+        </PlaceItem>
+      </PlacesContainer>
+    </StyledPlaces>
+  );
+}
+const StyledPlaces = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 11.75rem;
+  background-color: #41414d;
+  padding: 1.75rem 0;
+  gap: 1.375rem;
+`;
+const PlacesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  gap: 2.75rem;
+  width: 18.25rem;
+  height: 5.625rem;
+`;
+const StyledTitle = styled.div`
+  width: 5.8125rem;
+  height: 1.25rem;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.0625rem;
+  line-height: 1.25rem;
+  text-align: center;
+  color: #ffffff;
+`;
+const PlaceItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+  width: 4.25rem;
+  height: 5.625rem;
+`;
+const StyledFont = styled.span`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 0.8125rem;
+  line-height: 135%;
+  text-align: center;
+  color: #e2e2e2;
+`;
---
+++ src/components/Home/SubSlider.jsx
@@ -0,0 +1,262 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as ArrowIcon } from '../../assets/logos/icon_화살표.svg';
+import { ReactComponent as ExhibitIcon } from '../../assets/logos/icon_이번주공연전시.svg';
+import { ReactComponent as TicketIcon } from '../../assets/logos/icon_티켓오픈.svg';
+import { ReactComponent as NoticeIcon } from '../../assets/logos/icon_공지사항.svg';
+import { ReactComponent as MediaIcon } from '../../assets/logos/icon_미디어.svg';
+import { ReactComponent as LocationIcon } from '../../assets/logos/icon_위치_W.svg';
+import { ReactComponent as CalenderIcon } from '../../assets/logos/icon_캘린더_W.svg';
+
+function SubSlider({ media, exhibit, notice, ticket, title, moreInfo }) {
+  return (
+    <StyledSubSlider>
+      <SubHeader
+        media={media}
+        exhibit={exhibit}
+        notice={notice}
+        ticket={ticket}
+        title={title}
+        moreInfo={moreInfo}
+      ></SubHeader>
+      <StyledSubSliderCards>
+        {/* 예시 */}
+        <SubSliderCard
+          src={'/posters/4.드림어빌리티_포스터.jpg'}
+          tag="전시"
+          title={'뮤지컬 브로드웨이 콘서트입니다'}
+          endDate={'2022.03.10'}
+          startDate={'2022.02.15'}
+        ></SubSliderCard>
+      </StyledSubSliderCards>
+    </StyledSubSlider>
+  );
+}
+const SubHeader = ({ media, exhibit, notice, ticket, title, moreInfo }) => {
+  return (
+    <StyledSubHeader>
+      {media && <MediaIcon></MediaIcon>}
+      {notice && <NoticeIcon></NoticeIcon>}
+      {exhibit && <ExhibitIcon></ExhibitIcon>}
+      {ticket && <TicketIcon></TicketIcon>}
+      <StyledSlideInfo>
+        <StyledSlideName>{title}</StyledSlideName>
+        <StyledMoreInfo>
+          <StyledMoreInfoContent>{moreInfo ? moreInfo : '더보기'}</StyledMoreInfoContent>
+          <ArrowIcon />
+        </StyledMoreInfo>
+      </StyledSlideInfo>
+    </StyledSubHeader>
+  );
+};
+const SubSliderCard = ({ src, tag, title, endDate, startDate, locationDetail }) => {
+  return (
+    <StyledSubSliderCard>
+      <StyledImg src={src}>
+        <StyledType>
+          <StyledText>{tag}</StyledText>
+        </StyledType>
+      </StyledImg>
+      <StyledInfo>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledDateLocation>
+          <StyledDate>
+            <StyledCalenderIcon>
+              <CalenderIcon />
+            </StyledCalenderIcon>
+            <StyledDateText>
+              {startDate} - {endDate}
+            </StyledDateText>
+          </StyledDate>
+          <StyledLocation>
+            <StyledLocationIcon>
+              <LocationIcon />
+            </StyledLocationIcon>
+            <StyledLocationText>{locationDetail}</StyledLocationText>
+          </StyledLocation>
+        </StyledDateLocation>
+      </StyledInfo>
+    </StyledSubSliderCard>
+  );
+};
+const StyledSubSlider = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0rem;
+  gap: 1.5rem;
+  width: 22.1875rem;
+  height: 21.1875rem;
+`;
+const StyledSubHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 0.625rem;
+  width: 20.9375rem;
+  height: 1.375rem;
+`;
+const StyledSlideInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 0rem;
+  gap: 5.1875rem;
+  width: 18.9375rem;
+  height: 1.375rem;
+`;
+const StyledSlideName = styled.p`
+  height: 1.375rem;
+  width: 7.3125rem;
+  letter-spacing: -0.0063rem;
+  font-weight: 700;
+  font-size: 1.0625rem;
+  line-height: 1.25rem;
+  color: #ffffff;
+  text-shadow: 0rem 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+const StyledMoreInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.4375rem;
+  width: 6.4375rem;
+  height: 1.375rem;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+const StyledMoreInfoContent = styled.div`
+  width: 5.375rem;
+  height: 1.375rem;
+  font-size: 0.9375rem;
+  line-height: 1.125rem;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  color: #e2e2e2;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+const StyledSubSliderCards = styled.section`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 0.75rem;
+  width: 21.875rem;
+  height: 18.3125rem;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  white-space: pre;
+  overflow-y: hidden;
+  overflow-x: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+const StyledSubSliderCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.75rem;
+  width: 9.375rem;
+  height: 18.3125rem;
+`;
+
+const StyledImg = styled.div`
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  width: 9.375rem;
+  height: 11.9375rem;
+  border-radius: 0.3125rem;
+  position: relative;
+`;
+const StyledType = styled.div`
+  position: absolute;
+  width: 3.4375rem;
+  height: 1.6875rem;
+  left: 5.375rem;
+  top: 9.6875rem;
+  background: #41414d;
+  padding: 0.25rem;
+  text-align: center;
+`;
+const StyledText = styled.div`
+  width: 1.4375rem;
+  height: 1.1875rem;
+  font-weight: 500;
+  font-size: 0.8125rem;
+  line-height: 143.52%;
+  color: #ffffff;
+  display: inline-block;
+`;
+const StyledInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0rem;
+  gap: 0.3125rem;
+  width: 9.375rem;
+  height: 4.375rem;
+`;
+const StyledTitle = styled.p`
+  width: 9.375rem;
+  white-space: pre-line;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.9375rem;
+  line-height: 135%;
+  display: flex;
+  align-items: center;
+  color: #e2e2e2;
+`;
+const StyledDateLocation = styled.div`
+  width: 9.375rem;
+  height: 2.8125rem;
+`;
+const StyledDate = styled.div`
+  width: 9.375rem;
+  height: 1.375rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.25rem;
+`;
+const StyledLocation = styled(StyledDate)`
+  height: 1.25rem;
+  margin-top: 0.125rem;
+`;
+const StyledCalenderIcon = styled.div`
+  width: 1.25rem;
+  height: 1.375rem;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
+const StyledLocationIcon = styled(StyledCalenderIcon)`
+  height: 1.25rem;
+`;
+
+const StyledLocationText = styled.p`
+  width: 5.375rem;
+  height: 0.8125rem;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 0.6875rem;
+  line-height: 0.8125rem;
+  color: #e2e2e2;
+`;
+const StyledDateText = styled(StyledLocationText)`
+  width: 7.125rem;
+  height: 0.8125rem;
+`;
+export { SubSlider, SubHeader };
